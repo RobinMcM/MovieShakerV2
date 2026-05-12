8b0498d613a9f211324706268e801ba378746d37
RobinMcM <robin@dcs.uk.net>
2026-05-12 15:20:40 +0100

    fix error

--- web/src/components/project/RushesViewer.tsx
+++ web/src/components/project/RushesViewer.tsx
@@ -1251,7 +1251,7 @@ export function RushesViewer({ projectId, onAssetsChanged }: RushesViewerProps) 
           onConfirm={() => { pendingDelete.onConfirm(); setPendingDelete(null); }}
           onCancel={() => setPendingDelete(null)}
         />
-      }}
+      )}
 
       {/* Toolbar */}
       <div className="flex items-center gap-3 px-4 py-2 border-b border-border flex-shrink-0">

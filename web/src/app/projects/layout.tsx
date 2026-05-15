5bbfee4a8c1428fafd919f7f41aaa8df2b3b62a6
RobinMcM <robin@dcs.uk.net>
2026-05-16 00:27:58 +0100

    update error

--- web/src/app/projects/layout.tsx
+++ web/src/app/projects/layout.tsx
@@ -3,17 +3,11 @@
 import { useEffect, useState } from "react";
 import { ProjectSidebarNav } from "@/components/project/ProjectSidebarNav";
 import { cn } from "@/lib/utils";
-import { api } from "@/lib/api";
 
 const SIDEBAR_STORAGE_KEY = "movieshaker:project-nav-visible";
 
-interface ProfileData {
-    ai_credits?: number;
-}
-
 export default function ProjectsLayout({ children }: { children: React.ReactNode }) {
     const [desktopNavVisible, setDesktopNavVisible] = useState(true);
-    const [aiCredits, setAiCredits] = useState<number | null>(null);
 
     useEffect(() => {
         const stored = window.localStorage.getItem(SIDEBAR_STORAGE_KEY);
@@ -24,20 +18,11 @@ export default function ProjectsLayout({ children }: { children: React.ReactNode
         window.localStorage.setItem(SIDEBAR_STORAGE_KEY, desktopNavVisible ? "true" : "false");
     }, [desktopNavVisible]);
 
-    useEffect(() => {
-        api.get<ProfileData>("/profile/")
-            .then((p) => setAiCredits(p.ai_credits ?? 0))
-            .catch(() => setAiCredits(0));
-    }, []);
-
     return (
         <>
             <ProjectSidebarNav
                 scripts={[]}
                 scriptsLoading={false}
-                aiCredits={aiCredits}
-                coproducerActive={false}
-                onCoproducerActivate={() => {}}
                 isVisible={desktopNavVisible}
                 onToggle={() => setDesktopNavVisible((prev) => !prev)}
             />
